e3f8825542eb2e9a086c09b2aca4f1fb463fbf75
sobao <ryota.kono@mazrica.com>
2022-09-19 12:11:10 +0900

    jsonでレスポンスを返せるようにした

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,5 +1,6 @@
 class QuestionsController < ApplicationController
   def index
+    @users = [{id:1,name:'hoge',email: 'hoge@hoge.hoge'}]
   end
 
   def show
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,14 @@
 Rails.application.routes.draw do
-  root to: 'questions#index'
-
-  post '/login', to: 'sessions#create'
-  delete '/logout', to: 'sessions#destroy'
-
-  resources :users, only: [:create]
-  resources :questions, only: [:index, :show, :create]
-  resources :answers, only: [:create]
-  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  defaults format: :json do
+    root to: 'questions#index'
+  
+    post '/login', to: 'sessions#create'
+    delete '/logout', to: 'sessions#destroy'
+  
+    resources :users, only: [:create]
+    resources :questions, only: [:index, :show, :create]
+    resources :answers, only: [:create]
+  end
 end
