f833bf1cb891a869c967df5130304d06dbb0aa3d
sobao <ryota.kono@mazrica.com>
2022-09-22 16:24:19 +0900

    UsersControllerに関するテストとコードを実装した

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,13 @@
 class UsersController < ApplicationController
   def create
+    if current_user.nil?
+      User.create(create_params)
+    end
+  end
+
+  private
+
+  def create_params
+    params.permit(:name, :email, :password, :password_confirmation)
   end
 end
--- spec/requests/users/create_spec.rb
+++ spec/requests/users/create_spec.rb
@@ -1,11 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe "POST /users", type: :request do
-  context 'ログアウト状態の場合' do
+  it "新規にユーザー登録ができる" do
+    params = { 
+      name: 'user_a',
+      email: 'user_a@example.com',
+      password: 'password',
+      password_confirmation: 'password'
+    }
 
-    it "新規にユーザー登録ができる" do
-      post "/users"
-      expect(response).to have_http_status(:success)
-    end
+    post "/users", params: params
+    expect(response).to have_http_status(:success)
+
+    user = User.first
+    expect(user.name).to eq(params[:name])
+    expect(user.email).to eq(params[:email])
   end
 end
