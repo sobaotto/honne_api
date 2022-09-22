1053d0e8e1fd99d624a266d84b5a73baf212b5b5
sobao <ryota.kono@mazrica.com>
2022-09-22 16:25:00 +0900

    AnswersControllerに関するテストとコードを実装した

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,4 +1,22 @@
 class AnswersController < ApplicationController
   def create
+    if current_user
+      question = Question.find(create_params[:question_id])
+
+      Answer.create(
+        text: create_params[:text],
+        question: question,
+        user: current_user
+      )
+      @message = '回答しました'
+    else
+      @message = 'ログインしてください'
+    end
+  end
+
+  private
+
+  def create_params
+    params.permit(:question_id, :text)
   end
 end
--- spec/requests/answers/create_spec.rb
+++ spec/requests/answers/create_spec.rb
@@ -1,12 +1,48 @@
 require 'rails_helper'
 
 RSpec.describe "POST /answers", type: :request do
-  # こういうユーザーの機能制限は、基本フロントで担保する？
-  context 'ログイン状態の場合' do
-    
-    it "新規に回答の投稿ができる" do
-      post "/answers"
-      expect(response).to have_http_status(:success)
+  let(:user_a) { FactoryBot.create(:user, name: 'user_a') }
+  let(:user_b) { FactoryBot.create(:user, name: 'user_b') }
+  let(:question) { FactoryBot.create(:question, user: user_b) }
+
+  describe '回答投稿機能' do
+    context 'user_aでログインしている場合' do
+      before :each do
+        login_params = { 
+          name: user_a.name,
+          email: user_a.email,
+          password: user_a.password
+        }
+        post "/login", params: login_params
+      end
+  
+      it "user_bの質問に、新規で回答できる" do
+        params = { question_id: question.id, text: 'これは、回答本文です。' }
+
+        post "/answers", params: params
+        expect(response).to have_http_status(:success)
+        
+        answer = Answer.find_by(question_id: question.id, user_id: user_a.id)
+        expect(answer.text).to eq(params[:text])
+
+        res = JSON.parse(response.body, symbolize_names: true)
+        expect(res[:message]).to eq('回答しました')
+      end
+    end
+
+    context 'ログアウト状態の場合' do
+      it "user_bの質問に、新規で回答できない" do
+        params = { question_id: question.id, text: 'これは、回答本文です。' }
+
+        post "/answers", params: params
+        expect(response).to have_http_status(:success)
+        
+        answer = Answer.find_by(question_id: question.id, user_id: user_a.id)
+        expect(answer).to eq(nil)
+
+        parsed_response = JSON.parse(response.body, symbolize_names: true)
+        expect(parsed_response[:message]).to eq('ログインしてください')
+      end
     end
   end
 end
