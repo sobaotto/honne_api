5e4fb1730386dbe8c109b65fa71986cf190e2803
sobao <ryota.kono@mazrica.com>
2022-09-22 16:23:30 +0900

    SessionsControllerに関するテストとコードを実装した

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,22 @@
 class SessionsController < ApplicationController
   def create
+    user = User.find_by(
+      name: session_params[:name],
+      email: session_params[:email]
+    )
+    
+    if user&.authenticate(session_params[:password])
+      session[:user_id] = user.id
+    end
   end
 
   def destroy
+    reset_session
+  end
+
+  private
+
+  def session_params
+    params.permit(:name, :email, :password)
   end
 end
--- spec/requests/sessions_spec.rb
+++ spec/requests/sessions_spec.rb
@@ -1,17 +1,39 @@
 require 'rails_helper'
 
 RSpec.describe "Sessions", type: :request do
-  describe "POST /create" do
-    it "ログイン状態になる" do
-      post "/login"
-      expect(response).to have_http_status(:success)
-    end
-  end
+  describe "ログイン機能" do
+    let(:user) { FactoryBot.create(:user) }
 
-  describe "DELETE /destroy" do
+
+    it "ログイン状態になる" do
+      login_params = { 
+        name: user.name,
+        email: user.email,
+        password: user.password
+      }
+      post "/login", params: login_params
+
+      expect(response).to have_http_status(:success)
+      
+      current_user = JSON.parse(response.body, symbolize_names: true)
+      expect(current_user[:name]).to eq(user.name)
+      expect(current_user[:email]).to eq(user.email)
+    end
+
     it "ログアウト状態になる" do
+      login_params = { 
+        name: user.name,
+        email: user.email,
+        password: user.password
+      }
+      
+      post "/login", params: login_params
+      expect(response).to have_http_status(:success)
+
       delete "/logout"
       expect(response).to have_http_status(:success)
+
+      # ログアウトをどうやってテストするのか分からない。
     end
   end
 end
