7777eb14d786bde535fb7bf3251ee73553d45760
sobao <ryota.kono@mazrica.com>
2022-09-28 10:57:56 +0900

    modelにバリデーションを追加した

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,4 +1,6 @@
 class Answer < ApplicationRecord
   belongs_to :user
   belongs_to :question
+
+  validates :text, presence: true, length: { minimum: 5, maximum: 10000, message: '5文字以上10000字以下で入力してください' }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
 
     has_many :questions
     has_many :answers
+
+    validates :name, presence: true, uniqueness: true, length: { minimum:2, maximum:30 }, format: { with: /\A[A-Za-z0-9_-]+\z/ }
+    validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP } 
 end
