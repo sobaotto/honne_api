e1ab6d2d0c3a5a274f25610b7ea73c6e81563f00
sobaotto <ryota.blog.201111@gmail.com>
2022-11-06 13:18:24 +0900

    回答をつけたときに、質問の状態を変更するメソッドを追加した

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -5,4 +5,17 @@ class Answer < ApplicationRecord
   belongs_to :question
 
   validates :text, presence: true, length: { minimum: 5, maximum: 10_000, message: '5文字以上10000字以下で入力してください' }
+
+  after_create :change_question_status
+
+  private
+
+  def change_question_status
+    begin
+      question_with_answer = Question.find_by!(id: question_id)
+      question_with_answer.update!(question_status: 'resolved')
+    rescue StandardError => e
+      e
+    end
+  end
 end
